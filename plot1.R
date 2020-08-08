1df25e70ac87e17c73453079b288b66bf96ed656
Spnzking <anurajkashyap1234@gmail.com>
2020-08-08 06:08:48 +0530

    submitted

---
+++ plot1.R
@@ -0,0 +1,6 @@
+power <- read.table("C:/Users/ANURAJ/Desktop/data/household_power_consumption.txt", sep = ";", skip = 1)
+names(power) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+subpower <- subset(power, power$Date == "1/2/2007" | power$Date == "2/2/2007")
+hist(as.numeric(as.character(subpower$Global_active_power)), col = "red", main = "Global Active Power", xlab = "Global Active Power(kilowatts)")
+dev.copy(png, "plot1.png", height = 480, width = 480)
+dev.off()
